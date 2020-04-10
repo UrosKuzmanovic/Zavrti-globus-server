acee786dea855559031d6c2c1913d13e9487057b
UrosKuzmanovic <uki.kuzma.96@gmail.com>
2020-04-10 03:15:54 +0200

    povezivanje sa bazom, deo api-ja

---
+++ api/cities.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const pool = require('../db/index');
+const routes = express.Router();
+
+routes.get('/countries', async (req, res) => {
+    try {
+        res.json(await new Promise((resolve, reject) => {
+            pool.query('select distinct country from city order by country asc', (err, results) => {
+                if (err) {
+                    return reject(err);
+                } else {
+                    return resolve(results);
+                }
+            });
+        }));
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+module.exports = routes;
---
+++ middleware/logger.js
@@ -0,0 +1,6 @@
+const logger = (req, res, next) => {
+    
+    next();
+};
+
+module.exports = logger;
